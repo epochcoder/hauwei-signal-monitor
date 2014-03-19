89ac410254d364fccd0c59eacd843a07358c6b67
Willie Scholtz <williescholtz@gmail.com>
2014-03-20 00:41:34 +0200

    small bug fixes

--- signal-monitor.js
+++ signal-monitor.js
@@ -4,6 +4,7 @@ var sr = require('./signal-request.js').SignalRetriever,
 
 // constants
 var INTERVAL = 500,
+    DEBUG = true,
     URL = 'http://mifi.afrihost.com/api/monitoring/status';
 
 // app variables
@@ -14,7 +15,7 @@ board = new five.Board({
   port: "COM12"
 });
 
-// mapping function
+// mapping function (taken from arduino)
 function map(x, minIn, maxIn, minOut, maxOut) {
   return (x - minIn) * (maxOut - minOut) / (maxIn - minIn) + minOut; 
 }
@@ -33,30 +34,38 @@ board.on('ready', function() {
         return;
       }
 
-      var strength = parseInt(signal.SignalStrength[0], 10), ledStrength;
+      var strength = parseInt(signal.SignalStrength[0], 10), 
+          ledStrength, rounded, fraction, mapped;
 
       if (!previousReading || previousReading !== strength) {
-        previousReading = strength; 
-        
         // switch correct leds on/off
-        ledStrength = map(previousReading, 0, 100, 1, leds.length);
+        ledStrength = map(strength, 0, 100, 1, leds.length);
 
         // log new strength
-        console.log('1. reading:' + previousReading + ', mapped:' + ledStrength);
+        DEBUG && console.log('1. reading:' + strength + ', mapped:' + ledStrength);
 
-        // switch on/off any leds
-        leds.each(function(led, i) {
-          led[(i + 1) < ledStrength ? 'on' : 'off'].call(this);
-        });
+        if (strength === 0) {
+          //do something interesting, put everything off for now
+          leds.off();
+        } else {
+          // switch on/off any leds
+          leds.each(function(led, i) {
+            led[(i + 1) < ledStrength ? 'on' : 'off'].call(this);
+          });
 
-        // now show fractional strength
-        var rounded = Math.floor(ledStrength), 
-            fraction = ledStrength - rounded,
-            mapped = Math.floor(map(fraction, 0, 1, 1, 255));
-         
-        // log and set   
-        console.log('2. rounded: ' + rounded + ', fraction: ' + fraction + ', mapped:' + mapped);
-        leds[rounded].brightness(mapped);
+          // now show fractional strength
+          rounded = Math.floor(ledStrength), 
+          fraction = ledStrength - rounded,
+          mapped = Math.floor(map(fraction, 0, 1, 1, 255));
+
+          if (fraction > 0) {
+            // log and set  
+            DEBUG && console.log('2. rounded: ' + rounded + ', fraction: ' + fraction + ', mapped:' + mapped);
+            leds[rounded].brightness(mapped);
+          }
+        }
+
+        previousReading = strength; 
       }
 
       // poll again
